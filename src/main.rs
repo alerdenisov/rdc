9e5a844fc356017625a7132f916796df6fefa5d1
Aler Denisov <aler.zampillo@gmail.com>
2020-09-06 16:09:12 +0300

    add docs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+#![deny(missing_docs)]
+#![deny(missing_debug_implementations)]
+
+//! # Rust Developer Challenge
+//! 
+//! ## Introduction
+//! RDC is a simple Rust application that provides HTTP endpoints and responds to requests for files. Everything was done in accordance with the assignment received from Finheaven at the recruitment stage.
+//! 
+//! ## Assigment
+//! Code up a simple http microservice that loads a JSON data structure like the one found below and responds with a .zip file whose contents are each of the source `url` named as `filename` within the final .zip archive.
+//!
+//! Your service should expose a URL and respond with data as soon as possible rather than make the user wait for the entire ZIP to be created first.
+//! 
+//! Please include instructions on how someone else can run/test your service in their own webserver or development environment.
+//! 
+//! ## Run and test
+//! 
+//! <TODO>
+//! 
+//! ## Implementation and decisions
+//! 
+//! <TODO>
+
+
+
 fn main() {
     println!("Hello, world!");
 }
